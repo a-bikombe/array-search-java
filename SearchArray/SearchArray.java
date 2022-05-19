ecfeba05b19cc6ed8f7b9d89282d3b882ecd4d15
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2022-05-19 18:30:50 -0400

    init

---
+++ SearchArray/SearchArray.java
@@ -0,0 +1,23 @@
+package SearchArray;
+
+import java.util.Random;
+
+public class SearchArray {
+
+    public static void main(String[] args) {
+        // valueArray create an array and some values
+        int[] valueArray = {7, 4, 8, 10};
+        // set the target values
+
+        // value[0] = place[0] - match
+        // set i to 0
+        int i = 0;
+        // loop through valueArray
+        for (i = 0; i < valueArray.length; i++) {    // increment i
+            System.out.println("Element [" + i + "] = " + valueArray[i]);    // print out the value
+            // if valueArray equals targetValue
+        }
+                
+    }
+
+}
